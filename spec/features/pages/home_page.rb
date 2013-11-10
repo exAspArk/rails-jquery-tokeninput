13ee43d3781a6fc32836d0dce37c0a5671429ec9
Evgeny Li <exaspark@gmail.com>
2013-11-10 22:23:34 +0400

    add first green test

--- spec/features/pages/home_page.rb
+++ spec/features/pages/home_page.rb
@@ -9,20 +9,35 @@ class HomePage < Struct.new(:path)
   def enter_character(key_code)
     tokeninput.click
     page.execute_script %Q{
-      var e = jQuery.Event('keydown');
-      e.which = #{ key_code };
-      $('##{ tokeninput_id }').val(String.fromCharCode(e.which));
-      $('##{ tokeninput_id }').trigger(e);
+      var e = jQuery.Event('keydown', { which: #{ key_code } });
+      $('##{ tokeninput['id'] }').val(String.fromCharCode(e.which));
+      $('##{ tokeninput['id'] }').trigger(e);
     }
+  end
+
+  def select_first_token
+    first('.token-input-selected-dropdown-item-facebook').click
+  end
+
+  def dropdown
+    find('div.token-input-dropdown-facebook')
+  end
+
+  def with_dropdown?
+    page.has_css?(".#{ dropdown['class'].split(' ').join('.') }")
+  end
+
+  def selected_token
+    find('ul.token-input-list-facebook')
+  end
+
+  def with_selected_token?
+    page.has_css?(".#{ selected_token['class'].split(' ').join('.') }")
   end
 
 private
 
   def tokeninput
-    find("##{ tokeninput_id }")
-  end
-
-  def tokeninput_id
-    'token-input-countries'
+    find('#token-input-countries')
   end
 end
--- spec/features/tokeninput_spec.rb
+++ spec/features/tokeninput_spec.rb
@@ -5,9 +5,17 @@ feature 'Tokeninput' do
   let(:home_page) { HomePage.new(root_path) }
   let(:country)   { Country.first }
 
-  scenario 'Add tokens', js: true do
-    home_page.visit_page.enter_character(74) # 'J'
+  scenario 'Adding token', js: true do
+    home_page.visit_page
 
-    expect(page).to have_content(country[:name])
+    home_page.enter_character(74) # 'J'
+
+    expect(home_page).to be_with_dropdown
+    expect(home_page.dropdown).to have_content(country[:name])
+
+    home_page.select_first_token
+
+    expect(home_page).to be_with_selected_token
+    expect(home_page.selected_token).to have_content(country[:name])
   end
 end
